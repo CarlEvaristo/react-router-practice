5a004d4a4248e61a224c558225c5b3d5030c12d7
CarlEverest78 <thepythoneer@gmail.com>
2022-09-08 10:52:57 +0200

    add custom hamberger menu - pure css

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,33 @@ import Detail from "./Detail"
 import { useLocation } from "react-router-dom";
 
 function App() { 
+    const [menuOn, setMenuOn] = React.useState(false)
+
     let location = useLocation();
     location = (location.pathname.split("/"))[1]
     const active = path => path === "/"+location ? "active" : ""
+
+    function toggleMenu() {
+        setMenuOn(prev => !prev)
+    }
+
     return (
         <div className="container">
             <nav>
                 <h1 className="logoTitle">Shoply</h1>
-                <Link to="/" className={`menuItem marginLeft ${active("/")}`}>Home</Link>
-                <Link to="/products" className={`menuItem ${active("/products")}`}>Products</Link>
+                <div className={`menu ${ menuOn ? "moveDown" : undefined }`} >
+                    <Link to="/" className={`menuItem ${active("/")}`}>Home</Link>
+                    <Link to="/products" className={`menuItem ${active("/products")}`}>Products</Link>
+                </div>
+
+                <div className={`hamburgerBtn`} onClick={toggleMenu}>
+                    <div className={`hamburgerBar ${menuOn ? "topBar" : undefined}`}></div>
+                    <div className={`hamburgerBar ${menuOn ? "middleBar" : undefined}`}></div>
+                    <div className={`hamburgerBar ${menuOn ? "bottomBar" : undefined}`}></div>
+                </div>
             </nav>
 
-            <main>
+            <main className={menuOn ? "moveDown" : undefined} >
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route path="/products" element={<Products />} />
